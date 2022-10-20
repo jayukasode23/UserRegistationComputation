4a7744479fd58471c523c17b043ad5297c693b02
jayukasode23 <jayukasode2398@gmail.com>
2022-10-20 20:34:13 +0530

    Added User Number Password

---
+++ NumberPassword.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProgram
+{
+    public class NumberPassword
+    {
+        public string REGEX_PASSWORD = "^[A-Z]{1}[0-9]{8}$";
+
+        public bool ValidPassword(string password)
+        {
+            return Regex.IsMatch(password, REGEX_PASSWORD);
+
+
+        }
+    }
+}
