9300d86a9a32d0cf0d23d40704a2bcbd048ca971
jayukasode23 <jayukasode2398@gmail.com>
2022-10-20 20:26:03 +0530

    Added User First Name

---
+++ FirstName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProgram
+{
+    public class FirstName
+    {
+        public static string REGEX_FIRSTNAME = "^[A-Z]{1}[a-z]{3}$";
+        public bool ValidateFirstName(string firstname)
+        {
+            return Regex.IsMatch(firstname, REGEX_FIRSTNAME);
+        }
+    }
+}
