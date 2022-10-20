0051d81c0bbfc8ad2eeeaa827bb161db3f18e569
jayukasode23 <jayukasode2398@gmail.com>
2022-10-20 20:27:07 +0530

    Added User Last Name

---
+++ LastName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProgram
+{
+    public class LastName
+    {
+        public static string REGEX_LASTNAME = "^[A-Z]{1}[a-z]{5}$";
+        public bool ValidateLastName(string Lastname)
+        {
+            return Regex.IsMatch(Lastname, REGEX_LASTNAME);
+        }
+    }
+}
