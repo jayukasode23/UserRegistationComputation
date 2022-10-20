ed28bd0075c56d90f7c3d07c2888a95038f2ccf5
jayukasode23 <jayukasode2398@gmail.com>
2022-10-20 20:35:30 +0530

    Added User Special Character Password

---
+++ SpecialCharacter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProgram
+{
+    public class SpecialCharacter
+    {
+        public static string REGEX_PASSWORD = "^[A-Z]{1}[a-z]{4}[0-9]{2}['@'][A-Z]{1}[a-z]{2}[0-7]{2}$";
+        public bool ValidatePassword(string Password)
+        {
+            return Regex.IsMatch(Password, REGEX_PASSWORD);
+
+        }
+    }
+}
