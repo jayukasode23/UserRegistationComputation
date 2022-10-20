f17da934c84c0c0db66366cb8b8019ca4171163a
jayukasode23 <jayukasode2398@gmail.com>
2022-10-20 20:31:26 +0530

    Added User Pre-Defined Password

---
+++ Pre-DefinedPassword.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProgram
+{
+    public class Pre_DefinedPassword
+    {
+        public string REGEX_PASSWORD = "^[A-Z]{4}[a-z]{4}$";
+        public bool ValidPassword(string password)
+        {
+            return Regex.IsMatch(password, REGEX_PASSWORD);
+
+        }
+    }
+}
