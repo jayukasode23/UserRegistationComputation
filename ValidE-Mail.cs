e9ea38c9329cb8a49c38fb7cf39f537d8fd43fbb
jayukasode23 <jayukasode2398@gmail.com>
2022-10-20 20:28:22 +0530

    Added User Email

---
+++ ValidE-Mail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProgram
+{
+    public class ValidE_Mail
+    {
+        public string REGEX_EMAIL = "^[a-z]{10}[1-9]{4}@[a-z]{5}[.][a-z]{3}";
+
+        public bool ValidateEmail(string email)
+        {
+            return Regex.IsMatch(email, REGEX_EMAIL);
+        }
+    }
+}
+
