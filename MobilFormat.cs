44532661429907c10cbdf89be1726afab06fa66f
jayukasode23 <jayukasode2398@gmail.com>
2022-10-20 20:29:41 +0530

    Added User Mobile Format

---
+++ MobilFormat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProgram
+{
+    public class MobilFormat
+    {
+        public string REGEX_MOBILEFORMAT = "^[91]{2}[\\s][8-9]{1}[0-9]{9}$";
+        public bool ValidPhoneNumber(string number)
+        {
+            return Regex.IsMatch(number, REGEX_MOBILEFORMAT);
+        }
+    }
+}
