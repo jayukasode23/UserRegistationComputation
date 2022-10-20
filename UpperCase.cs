c985403e73179c8651e831da54f6268368221547
jayukasode23 <jayukasode2398@gmail.com>
2022-10-20 20:33:07 +0530

    Added User Upper Case Password

---
+++ UpperCase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProgram
+{
+    public class UpperCase
+    {
+        public string REGEX_PASSWORD = "^[A-Z]{5}[a-z]{3}$";
+
+        public bool ValidPassword(string password)
+        {
+            return Regex.IsMatch(password, REGEX_PASSWORD);
+
+
+        }
+    }
+}
